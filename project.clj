009239d82796328b36dd3b591861ba98e7d6f7aa
Oshan Wisumperuma <warunaoshan@gmail.com>
2026-01-03 14:13:47 +1100

    add rebl-readline

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/tools.logging "1.3.0"]
                  [ch.qos.logback/logback-classic "1.3.15"]
-                 [clj-kondo "2024.11.14"]]
+                 [clj-kondo "2024.11.14"]
+                 [com.bhauman/rebel-readline "0.1.5"]]
 
-  :aliases {"kondo" ["run" "-m" "clj-kondo.main" "--lint" "src:test" "--parallel"]})
+  :aliases {"kondo" ["run" "-m" "clj-kondo.main" "--lint" "src:test" "--parallel"]
+            "rebl" ["trampoline" "run" "-m" "rebel-readline.main"]})
