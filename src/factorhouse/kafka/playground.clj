66f6fd4e047ce0c15a6316a44e8b6d656efb4e3f
Oshan Wisumperuma <warunaoshan@gmail.com>
2026-01-23 15:52:54 +1100

    fizzbuzz

--- src/factorhouse/kafka/playground.clj
+++ src/factorhouse/kafka/playground.clj
@@ -34,7 +34,6 @@
 ;; juxt inout to a vector
 ((juxt even? pos-int? odd? #(< % 10)) 23)
 
-
 ;; comp right to left,
 ;; a(b(c(x)))
 ;;  pipe a()b()c()
@@ -44,7 +43,65 @@
 ;; -> first arg ->> last arg
 (-> {:name "oshan", :city "VIC" :age 31} (assoc :from "LK") (update :age inc))
 
-
 (mapcat vector [:v, :k, :h] [78, 4, 5])
 
 (reduce + 50 [8, 5])
+
+(group-by (juxt :broker :partition)  data/sizes)
+
+(sort-by  (juxt :size :broker) data/sizes)
+
+(dissoc {:broker 4, :part 7} :broker)
+
+(map-indexed (fn [i v] [(inc i) v]) ["l" "x" "z"])
+
+(into {} [[1 "a"] {2 "c"}])
+
+
+(assoc {:name "oshan"} :age 12)
+
+(for [a [1 2 4] b [:a :b]] [a b])
+
+(for [x (range 5)] [x])
+
+(for [x (range 5) :let [y (* x x)]] [y])
+
+
+(take 20 (for [x (range 100)
+               :let [y (inc x)
+                     fizz (zero? (mod y 3))
+                     buzz (zero? (mod y 5))]]
+           (cond (and fizz buzz) "FizzBuzz" fizz "Fizz" buzz "Buzz" :else y)))
+
+
+(reduce (fn [m k] (assoc m (keyword (str k)) (* k k))) {} (range 100))
+
+
+(into {} (map (fn [i] [(str i) "ok"])) (range 100))
+
+
+(filter #(zero? (mod % 3)) (range 100))
+
+(->> (range 1000) (filter #(zero? (mod % 3))) (take 20))
+
+(defn rule [n] (cond
+                 (zero? (mod n 15)) "FizzBuzz"
+                 (zero? (mod n 3)) "Fizz"
+                 (zero? (mod n 5)) "Buzz"))
+
+(->> (range 140 260)
+     (map (juxt identity #(rule %)))
+     (remove #(nil? (second %)))
+     (take 20))
+
+(merge {:l 78} {:j 5 :l 8})
+
+(keys {:j 78 :jn 9 :n 4})
+
+(vals {:k 8 :n 56})
+
+(get-in {:name "oz" :address {:city "vic"}} [:address :city])
+
+(assoc-in {:name "oz"} [:name] "wow")
+
+(update-in {:count 12} [:count] #(+ % 29))
